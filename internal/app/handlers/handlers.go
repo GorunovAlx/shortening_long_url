635281c34985bcc5e2a1b5496947fa2502df16ea
Алексей Горюнов <alexeygorunovpub@gmail.com>
2022-02-17 17:06:46 +0300

    checks tests

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -22,7 +22,7 @@ type Handler struct {
 }
 
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"8080"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
 }
 
