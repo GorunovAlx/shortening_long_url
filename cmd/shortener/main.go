3f122dcdf439c035ff1c17e9854d755b498ea8a7
Алексей Горюнов <alexeygorunovpub@gmail.com>
2022-02-22 16:15:36 +0300

    check tests with gin instead chi

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/GorunovAlx/shortening_long_url/internal/app/handlers"
 	"github.com/GorunovAlx/shortening_long_url/internal/app/storage"
@@ -10,7 +9,6 @@ import (
 
 func main() {
 	urlStorage := storage.NewShortURLStorage()
-	//handler := handlers.NewHandler(urlStorage)
 	r := handlers.RegisterRoutes(urlStorage)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(r.Run())
 }
--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -6,139 +6,81 @@ import (
 	"net/http"
 
 	valid "github.com/asaskevich/govalidator"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gin-gonic/gin"
 
 	"github.com/GorunovAlx/shortening_long_url/internal/app/storage"
 )
 
-// Handler is a structure containing the type of chi.Mux
-// and ShortURLRepo interface from storage package.
-type Handler struct {
-	*chi.Mux
-	Repo storage.ShortURLRepo
+func RegisterRoutes(repo storage.ShortURLRepo) *gin.Engine {
+	router := gin.Default()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.GET("/:shortURL", GetInitialLinkHandler(repo))
+	router.POST("/", CreateShortURLHandler(repo))
+	router.POST("/api/shorten", CreateShortURLJSONHandler(repo))
+
+	return router
 }
 
-// NewHandler returns a newly initialized Handler object that implements
-// the ShortURLRepo interface.
-func NewHandler(repo storage.ShortURLRepo) *Handler {
-	h := &Handler{
-		Mux:  chi.NewMux(),
-		Repo: repo,
-	}
-
-	h.Use(middleware.RequestID)
-	h.Use(middleware.RealIP)
-	h.Use(middleware.Logger)
-	h.Use(middleware.Recoverer)
-
-	h.Post("/", CreateShortURLHandler(repo))
-	//h.Get("/{shortURL}", GetInitialLinkHandler(repo))
-	h.Get("/", GetInitialLinkHandler(repo))
-	h.Post("/api/shorten", CreateShortURLJSONHandler(repo))
-
-	return h
-}
-
-func RegisterRoutes(repo storage.ShortURLRepo) http.Handler {
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-
-	r.Route("/", func(r chi.Router) {
-		r.Post("/", CreateShortURLHandler(repo))
-		r.Get("/{shortURL}", GetInitialLinkHandler(repo))
-		r.Post("/api/shorten", CreateShortURLJSONHandler(repo))
-	})
-
-	return r
-}
-
-func CreateShortURLJSONHandler(urlStorage storage.ShortURLRepo) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func CreateShortURLJSONHandler(urlStorage storage.ShortURLRepo) func(c *gin.Context) {
+	return func(c *gin.Context) {
 		var url storage.ShortURL
-		if err := json.NewDecoder(r.Body).Decode(&url); err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
+		if err := json.NewDecoder(c.Request.Body).Decode(&url); err != nil {
+			c.String(400, err.Error())
 		}
 
 		isURL := valid.IsURL(url.InitialLink)
 		if !isURL {
-			w.WriteHeader(400)
-			w.Write([]byte("Incorrect link"))
-			return
+			c.String(400, "Incorrect link")
 		}
 
 		shortURL, err := urlStorage.CreateShortURL(url.InitialLink)
 		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte(err.Error()))
+			c.String(400, err.Error())
 			return
 		}
 
 		res := storage.ShortURL{
 			ShortLink: shortURL,
 		}
-		resp, err := json.Marshal(res)
-		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte(err.Error()))
-			return
-		}
 
-		w.Header().Set("Content-type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		w.Write(resp)
+		c.JSON(http.StatusCreated, res)
 	}
 }
 
-// CreateShortURLHandler returns a http.HandlerFunc that processes the body of the request
-// which contains URL and returns a shortened link.
-func CreateShortURLHandler(urlStorage storage.ShortURLRepo) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		b, err := io.ReadAll(r.Body)
+func CreateShortURLHandler(urlStorage storage.ShortURLRepo) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		b, err := io.ReadAll(c.Request.Body)
 		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte("Incorrect request"))
-			return
+			c.String(400, err.Error())
 		}
 		if len(b) == 0 {
-			w.WriteHeader(400)
-			w.Write([]byte("Incorrect request"))
-			return
+			c.String(400, "Incorrect request")
 		}
 
 		shortURL, err := urlStorage.CreateShortURL(string(b))
 		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte(err.Error()))
-			return
+			c.String(400, err.Error())
 		}
 
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("http://localhost:8080/" + shortURL))
+		c.String(http.StatusCreated, "http://localhost:8080/"+shortURL)
 	}
 }
 
-// GetInitialLinkHandler returns a http.HandlerFunc that takes shortURL parameter
-// containing a short url and returns the initial link in the location header.
-func GetInitialLinkHandler(urlStorage storage.ShortURLRepo) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		shortURL := chi.URLParam(r, "shortURL")
+func GetInitialLinkHandler(urlStorage storage.ShortURLRepo) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		shortURL := c.Param("shortURL")
 		if shortURL == "" {
-			w.WriteHeader(400)
-			w.Write([]byte("short url was not sent"))
-			return
+			c.String(400, "short url was not sent")
 		}
 
 		link, err := urlStorage.GetInitialLink(shortURL)
 		if err != nil {
-			w.WriteHeader(400)
-			w.Write([]byte(err.Error()))
-			return
+			c.String(400, err.Error())
 		}
-
-		w.Header().Add("Location", link)
-		w.WriteHeader(http.StatusTemporaryRedirect)
+		c.Header("Location", link)
+		c.Status(http.StatusTemporaryRedirect)
 	}
 }
