2ba0e078fab3ceee84c9b537c89192583dc41daf
Алексей Горюнов <alexeygorunovpub@gmail.com>
2022-03-11 20:55:25 +0300

    work on errors from go vet test

--- internal/app/generators/shorturl_generator_test.go
+++ internal/app/generators/shorturl_generator_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestGenerateShortLink(t *testing.T) {
-	initialLink_1 := "https://bitfieldconsulting.com/golang/slower"
-	shortLink_1 := GenerateShortLink(initialLink_1)
+	initialLink1 := "https://bitfieldconsulting.com/golang/slower"
+	shortLink1 := GenerateShortLink(initialLink1)
 
-	initialLink_2 := "https://medium.com/scaled-agile-framework/exploring-key-elements-of-spotifys-agile-scaling-model-471d2a23d7ea"
-	shortLink_2 := GenerateShortLink(initialLink_2)
+	initialLink2 := "https://medium.com/scaled-agile-framework/exploring-key-elements-of-spotifys-agile-scaling-model-471d2a23d7ea"
+	shortLink2 := GenerateShortLink(initialLink2)
 
-	initialLink_3 := "https://medium.com/capital-one-tech/doing-well-by-doing-bad-writing-bad-code-with-go-part-1-2dbb96ce079a"
-	shortLink_3 := GenerateShortLink(initialLink_3)
+	initialLink3 := "https://medium.com/capital-one-tech/doing-well-by-doing-bad-writing-bad-code-with-go-part-1-2dbb96ce079a"
+	shortLink3 := GenerateShortLink(initialLink3)
 
-	assert.Equal(t, shortLink_1, "ZhvygENi")
-	assert.Equal(t, shortLink_2, "Cb68RzKG")
-	assert.Equal(t, shortLink_3, "jo9sYrzF")
+	assert.Equal(t, shortLink1, "ZhvygENi")
+	assert.Equal(t, shortLink2, "Cb68RzKG")
+	assert.Equal(t, shortLink3, "jo9sYrzF")
 }
